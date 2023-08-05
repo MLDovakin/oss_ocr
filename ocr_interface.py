d8fb37607041ef682a28741d081b5a57aa460798
MLDovakin <indiangamedevelop@gmail.com>
2023-08-05 16:47:41 +0300

    test

--- ocr_interface.py
+++ ocr_interface.py
@@ -5,31 +5,20 @@ import easyocr
 import os
 from PIL import Image
 import cv2
+from load_model import load_model, get_config, inference
+
 import numpy as np
 
+opt=get_config('./en_filtered_config_t.yaml')
+model=load_model('best_accuracy_t.pth',opt=opt)
 
-print(os.listdir('user_network'))
+
 def define_doc_state(doc):
-    reader = easyocr.Reader(['ru'],
-                            model_storage_directory='model',
-                            user_network_directory='user_network',
-                            recog_network='custom_example')
 
-    print(doc.name)
-    img = cv2.imread(doc.name, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img,(600,100),interpolation=Image.ANTIALIAS)
-    cv2.imwrite(doc.name, img)
-    result = reader.readtext(doc.name, allowlist='(),.ӔӕАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-',paragraph=False)
-    st.write(result[0][1])
+    img = Image.open(doc.name).convert('RGB').resize((600, 100)).convert('L')
+    q = inference(model, img, opt)
 
-    top_left = tuple(result[0][0][0])
-    bottom_right = tuple(result[0][0][2])
-    text = result[0][1]
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-    img = cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 3)
-    img = cv2.putText(img, 'text', top_left, font, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-
+    st.write(q)
     st.image(img, caption='Detection')
 
 uploaded_file = st.file_uploader("Выберите файл", type=[".JPG", ".jpg", ".png",], accept_multiple_files=False)
@@ -39,7 +28,7 @@ if uploaded_file:
     save_folder = os.getcwd()
     save_path = uploaded_file.name
     print(save_path)
-    img_arr = cv2.imdecode(np.frombuffer(uploaded_file.getvalue(), np.uint8), cv2.IMREAD_COLOR)
+    img_arr = cv2.imdecode(np.frombuffer(uploaded_file.getvalue(), np.uint8), cv2.IMREAD_UNCHANGED,)
     cv2.imwrite(save_path, img_arr)
 
     if define_button_state:
