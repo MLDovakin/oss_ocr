daa19d62c27b239ff9c4a4031bc128e3cb708221
Dragonborn <78375175+MLDovakin@users.noreply.github.com>
2023-09-26 04:35:25 +0300

    Update ocr_interface.py

--- ocr_interface.py
+++ ocr_interface.py
@@ -54,3 +54,17 @@ if uploaded_file:
 st.title('Введите текст для проверки орфографии')
 
 
+hobj = HunSpell()
+#hobj.add_dic('./os_dict/os_OS.dic')
+words_st = st.text_input('Текст на осетинском/дигорском ', 'Ӕлгъуыз')
+
+if st.button('Исправить'):
+
+    if words_st:
+        words = words_st.split(' ')
+        for i in range(len(words)):
+            sp = hobj.spell(words[i])
+            if sp != True:
+                print(st.write(f"слово с ошибкой : :red[{words[i]}]"))
+                words[i] = hobj.suggest(words[i])[0]
+        st.write(' '.join(words))
