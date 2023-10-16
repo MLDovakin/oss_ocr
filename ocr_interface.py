85ea7235b809161c9cef9c05791eabf0120e5d7e
Dragonborn <78375175+MLDovakin@users.noreply.github.com>
2023-10-16 18:49:13 +0300

    Update ocr_interface.py

--- ocr_interface.py
+++ ocr_interface.py
@@ -132,7 +132,7 @@ def prep_pdf(pdf_reader):
 
 if pdf_uploaded_file:
     
-    if pdf_uploaded_file.name[-4:] == '.pdf':
+    if pdf_uploaded_file.name.endwith('.pdf'):
         
         open('dest.txt', 'a').close()
         with open(pdf_uploaded_file.name, 'wb') as f:
